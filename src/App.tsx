65da1e79409b6e3248134068cf484085afea42c6
Adrian Litewski <litewskidev@gmail.com>
2023-02-15 23:18:35 +0100

    add types & render books

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
 import { FC, useState } from 'react';
 
-const booksData = [
+interface Book {
+  id: string;
+  title: string;
+  author: string;
+  price: number;
+};
+
+const booksData: Book[] = [
   { id: 'te1314derw', title: 'Of Mice and Men', author: 'John Steinbeck', price: 25 },
   { id: 't32s14dzdw', title: 'East of Eden', author: 'John Steinbeck', price: 30 }
 ];
 
 const App: FC = () => {
 
-  const [books, setBooks] = useState(booksData);
+  const [books, setBooks] = useState<Book[]>(booksData);
 
   return(
     <div className="container">
       <header>
         <h1>Books App</h1>
       </header>
+      {books.map((book: Book) => <span>{book.title}</span>)}
     </div>
   );
 };
