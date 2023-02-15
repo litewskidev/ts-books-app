1f15db46879736c8c56741d717e9fc87ef3a17c2
Adrian Litewski <litewskidev@gmail.com>
2023-02-16 00:03:11 +0100

    add handleSubmit & randomID FUNCS

--- src/components/AddBookForm/AddBookForm.tsx
+++ src/components/AddBookForm/AddBookForm.tsx
@@ -1,4 +1,6 @@
-import { FC, useState } from "react";
+import { FC, FormEvent, useState } from "react";
+import { Book } from "../../interfaces/Book.interface";
+import { randomID } from "../../utils/randomID";
 
 type Props = {
 
@@ -10,8 +12,19 @@ const AddBookForm: FC<Props> = () => {
   const [author, setAuthor] = useState<string>('');
   const [price, setPrice] = useState<number>(0);
 
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const book: Book = {
+      id: randomID(10),
+      title,
+      author,
+      price
+    };
+    console.log(book);
+  };
+
   return(
-    <form className="add-book-form">
+    <form className="add-book-form" onSubmit={handleSubmit}>
       Title: <input onChange={e => setTitle(e.target.value)} type="text" />
       Author: <input onChange={e => setAuthor(e.target.value)} type="text" />
       Price: <input onChange={e => setPrice(parseInt(e.target.value))} type="number" />
